7ef742de53cd9cdcfe1f9eedf9d362bce005de82
GabrielMartinsJs <gabrieluzumaki2006@gmail.com>
2026-02-03 18:01:07 -0300

    feat: create docker image

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from dotenv import load_dotenv
+from dotenv import load_dotenv 
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent
 load_dotenv(BASE_DIR / '.env')
 
 from fastapi import FastAPI, Depends
-import uvicorn
+import uvicorn  
 from films.routes import router as films_router
 from characters.routes import router as characters_routes
 from app.dependencies import get_db
@@ -25,5 +25,5 @@ app.include_router(planets_routes)
 app.include_router(starships_routes)
 
 if __name__ == "__main__":
-    uvicorn.run(app, port=8000)
+    uvicorn.run(app, port=8000, host="0.0.0.0")
     
