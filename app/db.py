0081aabcab9a647f1ac6694fb39b87b900aef2c9
Jubz-Cherry <juliapnascimento15@gmail.com>
2026-02-05 19:52:02 -0300

    fixed infos in db

--- app/db.py
+++ app/db.py
@@ -7,9 +7,14 @@ DATABASE_URL = None
 USE_CLOUD = os.getenv("USE_CLOUD", "false").lower() == "true"
 
 if USE_CLOUD:
-    # nome exato do banco
-    DB_NAME = "starwars-db"
-    DATABASE_URL = f"postgresql+psycopg://postgres:Postgres10201191%21@/{DB_NAME}?host=/cloudsql/deep-burner-486320-d9:southamerica-east1:starwars-post"
+    # Configuração para Cloud SQL via Unix socket
+    DB_NAME = "starwars-db"               # nome exato do banco
+    DB_USER = "postgres"                  # usuário do banco
+    DB_PASS = "Postgres10201191%21"      # senha já percent-encoded
+    DB_HOST = "/cloudsql/deep-burner-486320-d9:southamerica-east1:starwars-post"  # Unix socket do Cloud SQL
+
+    # URL de conexão para SQLAlchemy usando Unix socket
+    DATABASE_URL = f"postgresql+psycopg://{DB_USER}:{DB_PASS}@/{DB_NAME}?host={DB_HOST}"
 
 else:
     DATABASE_URL = os.getenv("DATABASE_URL_LOCAL")
