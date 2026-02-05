95193282695f5f3ff423b937db22c34cc032a6f1
Jubz-Cherry <juliapnascimento15@gmail.com>
2026-02-05 19:27:42 -0300

    fixed db

--- app/db.py
+++ app/db.py
@@ -7,7 +7,10 @@ DATABASE_URL = None
 USE_CLOUD = os.getenv("USE_CLOUD", "false").lower() == "true"
 
 if USE_CLOUD:
-    DATABASE_URL = os.getenv("DATABASE_URL_CLOUD")
+    # nome exato do banco
+    DB_NAME = "starwars-db"
+    DATABASE_URL = f"postgresql+psycopg://postgres:Postgres10201191%21@/{DB_NAME}?host=/cloudsql/deep-burner-486320-d9:southamerica-east1:starwars-post"
+
 else:
     DATABASE_URL = os.getenv("DATABASE_URL_LOCAL")
 
